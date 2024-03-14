0f36cc607ce5ae263359e3e423e705be76a0dc36
sarapalaciod <sarapalaciod@udea.edu.co>
2024-03-13 19:17:58 -0500

    Creación de la clase Protesis Cadera heredada de la clase Implante medico

--- quiz1_isaV_SaraP.py
+++ quiz1_isaV_SaraP.py
@@ -32,3 +32,11 @@ class ImplanteMedico:
 
     def registrar_mantenimiento(self, fecha_mantenimiento):
         self._fecha_ultimo_mantenimiento = fecha_mantenimiento
+
+class ProtesisCadera(ImplanteMedico):
+    def __init__(self, tipo, material, tamaño, fijacion):
+        super().__init__(tipo, material, tamaño)
+        self._fijacion = fijacion
+
+    def __str__(self):
+        return f'ID: {self._id}, Tipo: {self._tipo}, Material: {self._material}, Tamaño: {self._tamaño}, Fijación: {self._fijacion}, Fecha de fabricación: {self._fecha_fabricacion.strftime("%Y-%m-%d")}, Fecha de vencimiento: {self._obtener_fecha_vencimiento()}, Paciente: {self._paciente}, Fecha de implantación: {self._fecha_implantacion}, Médico responsable: {self._medico_responsable}, Estado del implante: {self._estado_implante}, Última revisión: {self._fecha_ultima_revision}, Último mantenimiento: {self._fecha_ultimo_mantenimiento}'
